6009523d779dd73d9a35c8062e95c6b891005cf5
Stephan Jaeckel <jaeckel@sjc-wireless.com>
2023-11-29 10:17:37 +0100

    Return value optimization in arrayant::is_valid

--- src/qd_arrayant.cpp
+++ src/qd_arrayant.cpp
@@ -1315,61 +1315,61 @@ std::string quadriga_lib::arrayant<dtype>::is_valid(bool quick_check) const
                       check_ptr[7] == coupling_re.memptr() &&
                       check_ptr[8] == coupling_im.memptr();
     if (quick_check)
-        return "";
+        return std::string("");
 
     // Perform a deep check
     if (e_theta_re.n_elem == 0 || e_theta_im.n_elem == 0 || e_phi_re.n_elem == 0 || e_phi_im.n_elem == 0 || azimuth_grid.n_elem == 0 || elevation_grid.n_elem == 0)
-        return "Missing data for any of: e_theta_re, e_theta_im, e_phi_re, e_phi_im, azimuth_grid, elevation_grid";
+        return std::string("Missing data for any of: e_theta_re, e_theta_im, e_phi_re, e_phi_im, azimuth_grid, elevation_grid");
 
     unsigned long long n_elevation = e_theta_re.n_rows;
     unsigned long long n_azimuth = e_theta_re.n_cols;
     unsigned long long n_elements = e_theta_re.n_slices;
 
     if (e_theta_im.n_rows != n_elevation || e_theta_im.n_cols != n_azimuth || e_theta_im.n_slices != n_elements)
-        return "Sizes of 'e_theta_re', 'e_theta_im', 'e_phi_re', 'e_phi_im' do not match.";
+        return std::string("Sizes of 'e_theta_re', 'e_theta_im', 'e_phi_re', 'e_phi_im' do not match.");
 
     if (e_phi_re.n_rows != n_elevation || e_phi_re.n_cols != n_azimuth || e_phi_re.n_slices != n_elements)
-        return "Sizes of 'e_theta_re', 'e_theta_im', 'e_phi_re', 'e_phi_im' do not match.";
+        return std::string("Sizes of 'e_theta_re', 'e_theta_im', 'e_phi_re', 'e_phi_im' do not match.");
 
     if (e_phi_im.n_rows != n_elevation || e_phi_im.n_cols != n_azimuth || e_phi_im.n_slices != n_elements)
-        return "Sizes of 'e_theta_re', 'e_theta_im', 'e_phi_re', 'e_phi_im' do not match.";
+        return std::string("Sizes of 'e_theta_re', 'e_theta_im', 'e_phi_re', 'e_phi_im' do not match.");
 
     if (azimuth_grid.n_elem != n_azimuth)
-        return "Number of elements in 'azimuth_grid' does not match number of columns in pattern data.";
+        return std::string("Number of elements in 'azimuth_grid' does not match number of columns in pattern data.");
 
     if (elevation_grid.n_elem != n_elevation)
-        return "Number of elements in 'elevation_grid' does not match number of rows in pattern data.";
+        return std::string("Number of elements in 'elevation_grid' does not match number of rows in pattern data.");
 
     bool error_code = false;
     for (const dtype *val = azimuth_grid.begin(); val < azimuth_grid.end(); ++val)
         error_code = *val < dtype(-3.1415930) || *val > dtype(3.1415930) ? true : error_code;
     if (error_code)
-        return "Values of 'azimuth_grid' must be between -pi and pi (equivalent to -180 to 180 degree).";
+        return std::string("Values of 'azimuth_grid' must be between -pi and pi (equivalent to -180 to 180 degree).");
 
     for (const dtype *val = elevation_grid.begin(); val < elevation_grid.end(); ++val)
         error_code = *val < dtype(-1.5707965) || *val > dtype(1.5707965) ? true : error_code;
     if (error_code)
-        return "Values of 'elevation_grid' must be between -pi/2 and pi/2 (equivalent to -90 to 90 degree).";
+        return std::string("Values of 'elevation_grid' must be between -pi/2 and pi/2 (equivalent to -90 to 90 degree).");
 
     if (!azimuth_grid.is_sorted())
-        return "Values of 'azimuth_grid' must be sorted in ascending order.";
+        return std::string("Values of 'azimuth_grid' must be sorted in ascending order.");
 
     if (!elevation_grid.is_sorted())
-        return "Values of 'elevation_grid' must be sorted in ascending order.";
+        return std::string("Values of 'elevation_grid' must be sorted in ascending order.");
 
     if (!element_pos.empty() && (element_pos.n_rows != 3 || element_pos.n_cols != n_elements))
-        return "Size of 'element_pos' must be either empty or match [3, n_elements]";
+        return std::string("Size of 'element_pos' must be either empty or match [3, n_elements]");
 
     if (!coupling_re.empty() && coupling_re.n_rows != n_elements)
-        return "'Coupling' must be a matrix with rows equal to number of elements";
+        return std::string("'Coupling' must be a matrix with rows equal to number of elements");
 
     if (coupling_re.empty() && !coupling_im.empty())
-        return "Imaginary part of coupling matrix (phase component) defined without real part (absolute component)";
+        return std::string("Imaginary part of coupling matrix (phase component) defined without real part (absolute component)");
 
     if (!coupling_im.empty() && (coupling_im.n_rows != n_elements || coupling_im.n_cols != coupling_re.n_cols))
-        return "'coupling_im' must be empty or its size must match 'coupling_re'";
+        return std::string("'coupling_im' must be empty or its size must match 'coupling_re'");
 
-    return "";
+    return std::string("");
 }
 
 // ARRAYANT METHOD : Validates correctness of the member functions and initializes element positions and coupling matrix
@@ -1403,7 +1403,7 @@ std::string quadriga_lib::arrayant<dtype>::validate()
     check_ptr[7] = coupling_re.memptr();
     check_ptr[8] = coupling_im.memptr();
 
-    return "";
+    return std::string("");
 }
 
 // Instantiate templates
